21fafdc7de6d65a8770b942e4d7c228c33990f7d
Hansel <hanselcapillo@hotmail.com>
2020-01-08 23:22:30 -0500

    AngularFire2 - Comunicar nuestra aplicación de Angular con Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAjvWNce93qhlhDeYqdOSWfjfF3ebKxOBk",
+    authDomain: "ingreso-egreso-app-c38d8.firebaseapp.com",
+    databaseURL: "https://ingreso-egreso-app-c38d8.firebaseio.com",
+    projectId: "ingreso-egreso-app-c38d8",
+    storageBucket: "ingreso-egreso-app-c38d8.appspot.com",
+    messagingSenderId: "307492285495",
+    appId: "1:307492285495:web:4f11d87376099c812523b2",
+    measurementId: "G-RX2B8J2DM7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAjvWNce93qhlhDeYqdOSWfjfF3ebKxOBk",
+    authDomain: "ingreso-egreso-app-c38d8.firebaseapp.com",
+    databaseURL: "https://ingreso-egreso-app-c38d8.firebaseio.com",
+    projectId: "ingreso-egreso-app-c38d8",
+    storageBucket: "ingreso-egreso-app-c38d8.appspot.com",
+    messagingSenderId: "307492285495",
+    appId: "1:307492285495:web:4f11d87376099c812523b2",
+    measurementId: "G-RX2B8J2DM7"
+  }
 };
 
 /*
