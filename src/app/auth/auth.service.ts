0a4b5337da0d39011bba9f0f5735023956774cf8
Hansel <hanselcapillo@hotmail.com>
2020-01-11 12:55:37 -0500

    Crear documento de base de datos por cada usuario

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFirestore } from "@angular/fire/firestore";
 import { Router } from "@angular/router";
 import Swal from "sweetalert2";
 import * as firebase from "firebase";
 import { map } from "rxjs/operators";
+import { User } from "./user.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private afDB: AngularFirestore
+  ) {}
 
   initAuthListener() {
     this.afAuth.authState.subscribe((fbUSer: firebase.User) => {
@@ -17,11 +23,23 @@ export class AuthService {
     });
   }
 
-  crearUsuario(nombre: string, email: string, password: string) {
+  crearUsuario(name: string, email: string, password: string) {
     this.afAuth.auth
       .createUserWithEmailAndPassword(email, password)
       .then(resp => {
-        console.log(resp);
+        const user: User = {
+          uid: resp.user.uid,
+          nombre: name,
+          email: resp.user.email
+        };
+
+        this.afDB
+          .doc(`${user.uid}/usuario`)
+          .set(user)
+          .then(() => {
+            this.router.navigate(["/"]);
+          });
+
         this.router.navigate(["/"]);
       })
       .catch(error => {
