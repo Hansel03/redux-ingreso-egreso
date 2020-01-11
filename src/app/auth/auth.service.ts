6909a7a4805c5552ac8d69bfb25d5f478b35e2f4
Hansel <hanselcapillo@hotmail.com>
2020-01-11 11:58:51 -0500

    SweetAlert 2 para los errores

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { Router } from "@angular/router";
+import Swal from "sweetalert2";
 
 @Injectable({
   providedIn: "root"
@@ -17,6 +18,7 @@ export class AuthService {
       })
       .catch(error => {
         console.error(error);
+        Swal.fire("Error en el registro", error.message, "error");
       });
   }
 
@@ -29,6 +31,7 @@ export class AuthService {
       })
       .catch(error => {
         console.error(error);
+        Swal.fire("Error login", error.message, "error");
       });
   }
 }
