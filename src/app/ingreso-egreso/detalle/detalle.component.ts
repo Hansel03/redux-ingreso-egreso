b8fcef9c978b97886d9dfc5317e6f1997eedfc6f
Hansel <hanselcapillo@hotmail.com>
2020-01-16 18:40:38 -0500

    Mostrar todos los elementos de ingreso y egreso

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/app.reducer";
+import { IngresoEgresoModel } from "../ingreso-egreso.model";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-detalle',
-  templateUrl: './detalle.component.html',
+  selector: "app-detalle",
+  templateUrl: "./detalle.component.html",
   styles: []
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
+  items: IngresoEgresoModel[];
 
-  constructor() { }
+  suncription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.suncription = this.store
+      .select("ingresoEgreso")
+      .subscribe(ingresoEgreso => {
+        this.items = ingresoEgreso.items;
+      });
   }
 
+  borrarItem(uid: string) {}
+
+  ngOnDestroy() {
+    this.suncription.unsubscribe();
+  }
 }
