bfb6003a67b85e04a9882ebf9d4757c9fb5e546f
Hansel <hanselcapillo@hotmail.com>
2020-01-16 16:30:17 -0500

    Modelo ingreso egreso

---
+++ src/app/ingreso-egreso/ingreso-egreso.model.ts
@@ -0,0 +1,20 @@
+export class IngresoEgresoModel {
+  descripcion: string;
+  monto: string;
+  tipo: string;
+  uid?: string;
+
+  constructor(obj: DataObj) {
+    this.descripcion = (obj && obj.descripcion) || null;
+    this.monto = (obj && obj.monto) || null;
+    this.tipo = (obj && obj.tipo) || null;
+    this.uid = (obj && obj.uid) || null;
+  }
+}
+
+interface DataObj {
+  descripcion: string;
+  monto: string;
+  tipo: string;
+  uid?: string;
+}
