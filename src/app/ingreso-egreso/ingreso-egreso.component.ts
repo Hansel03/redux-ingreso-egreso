c9d2ef3a3cb2aa63a6ddf36cc7651657f8898b48
Hansel <hanselcapillo@hotmail.com>
2020-01-16 17:20:56 -0500

    Optimizaciones de loading y no grabar el UID del item

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { IngresoEgresoModel } from "./ingreso-egreso.model";
 import { IngresoEgresoService } from "./ingreso-egreso.service";
 import Swal from "sweetalert2";
+import { Subscription } from "rxjs";
+import { Store } from "@ngrx/store";
+import { AppState } from "../app.reducer";
+import {
+  ActivarLoadingAction,
+  DesactivarLoadingAction
+} from "../shared/ui.accions";
 
 @Component({
   selector: "app-ingreso-egreso",
   templateUrl: "./ingreso-egreso.component.html",
   styles: []
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
   forma: FormGroup;
   tipo = "ingreso";
+  loadingSubs: Subscription = new Subscription();
+  cargando: boolean;
 
-  constructor(public ingresoEgresoService: IngresoEgresoService) {}
+  constructor(
+    public ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit() {
+    this.loadingSubs = this.store
+      .select("ui")
+      .subscribe(ui => (this.cargando = ui.isLoading));
+
     this.forma = new FormGroup({
       descripcion: new FormControl("", Validators.required),
       monto: new FormControl(0, Validators.min(0))
@@ -23,6 +39,8 @@ export class IngresoEgresoComponent implements OnInit {
   }
 
   crearIngresoEgreso() {
+    this.store.dispatch(new ActivarLoadingAction());
+
     const ingresoEgreso = new IngresoEgresoModel({
       ...this.forma.value,
       tipo: this.tipo
@@ -30,11 +48,17 @@ export class IngresoEgresoComponent implements OnInit {
     this.ingresoEgresoService
       .crearIngresoEgreso(ingresoEgreso)
       .then(() => {
+        this.store.dispatch(new DesactivarLoadingAction());
         this.forma.reset({ monto: 0 });
         Swal.fire("Creado", ingresoEgreso.descripcion, "success");
       })
       .catch(err => {
+        this.store.dispatch(new DesactivarLoadingAction());
         console.log(err);
       });
+  }
+
+  ngOnDestroy() {
+    this.loadingSubs.unsubscribe();
   }
 }
