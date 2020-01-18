107f08af694a31066c21beb7c2d10eb2788aaee5
Hansel <hanselcapillo@hotmail.com>
2020-01-18 16:45:35 -0500

    Colocar el nombre del usuario activo

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/app.reducer";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
   styles: []
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  user: string;
 
-  constructor() { }
+  subcription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.subcription = this.store.select("auth").subscribe(usuarioLogin => {
+      if (usuarioLogin.user) {
+        this.user = usuarioLogin.user.nombre;
+      }
+    });
   }
 
+  ngOnDestroy() {
+    this.subcription.unsubscribe();
+  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { AuthService } from "src/app/auth/auth.service";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/app.reducer";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
   styles: []
 })
-export class SidebarComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+export class SidebarComponent implements OnInit, OnDestroy {
+  user: string;
 
-  ngOnInit() {}
+  subcription: Subscription = new Subscription();
+  constructor(
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {}
+
+  ngOnInit() {
+    this.subcription = this.store.select("auth").subscribe(usuarioLogin => {
+      if (usuarioLogin.user) {
+        this.user = usuarioLogin.user.nombre;
+      }
+    });
+  }
 
   logout() {
     this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.subcription.unsubscribe();
   }
 }
