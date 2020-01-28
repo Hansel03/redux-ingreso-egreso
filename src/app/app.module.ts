7fef9863e4bed2fe0b62d1d9839029eea9bfdabd
Hansel <hanselcapillo@hotmail.com>
2020-01-28 16:48:28 -0500

    Módulo Ingreso-Egreso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
 
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
@@ -11,36 +10,19 @@ import { StoreModule } from "@ngrx/store";
 import { appReducers } from "./app.reducer";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-//Graficas
-import { ChartsModule } from "ng2-charts";
-
 //Modulos perzonilizados
 import { AuthModule } from "./auth/auth.module";
-import { SharedModule } from "./shared/shared.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { IngresoEgresoComponent } from "./ingreso-egreso/ingreso-egreso.component";
-import { EstadisticaComponent } from "./ingreso-egreso/estadistica/estadistica.component";
-import { DetalleComponent } from "./ingreso-egreso/detalle/detalle.component";
 import { environment } from "../environments/environment";
-import { OrdenIngresoEgresoPipe } from "./ingreso-egreso/orden-ingreso-egreso.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    IngresoEgresoComponent,
-    EstadisticaComponent,
-    DetalleComponent,
-    OrdenIngresoEgresoPipe
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    SharedModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
@@ -48,9 +30,7 @@ import { OrdenIngresoEgresoPipe } from "./ingreso-egreso/orden-ingreso-egreso.pi
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production // Restrict extension to log-only mode
-    }),
-    ReactiveFormsModule,
-    ChartsModule
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
